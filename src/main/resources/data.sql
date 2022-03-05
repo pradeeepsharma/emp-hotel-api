3a8eda835de169f3f06b3fec7067ac1d1b4ca9a1
Pradeep <pradeeepsharma@gmail.com>
2022-03-05 11:28:49 +0530

    Hotel API

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO
-Hotel (id,name, address, floors)
+emp_db.hotel (id,name, address, floors)
 VALUES
 ('1', 'The Random Hotel', 'Somewhere unreachable', '102'),
 ('2', 'The Expensive Hotel', 'Something unaffordable', '34');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE hotel IF EXISTS;
+CREATE TABLE emp_db.hotel(
+    id INTEGER(10),
+    name VARCHAR(250),
+    address VARCHAR(300),
+    floors INTEGER(3),
+     PRIMARY KEY (id)
+);
+
